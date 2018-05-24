c095daa093f19ae7382178705d29b99b65506d05
Christos Matsoukas <chrismatsoukas@gmail.com>
2018-05-24 22:25:39 +0200

    create visualizers

---
+++ src/proto.py
@@ -0,0 +1,193 @@
+# DT2119, Lab 1 Feature Extraction
+
+import matplotlib.pylab as plt
+import numpy as np
+from scipy.fftpack import fft
+from scipy.fftpack.realtransforms import dct
+from scipy.signal import lfilter, hamming
+from scipy.spatial.distance import euclidean
+
+from tools import lifter, trfbank
+
+import pdb
+# Function given by the exercise ----------------------------------
+
+def mfcc(samples, winlen=20, winshift=10, preempcoeff=0.97, nfft=512, nceps=13, samplingrate=20000, liftercoeff=22):
+    """Computes Mel Frequency Cepstrum Coefficients.
+
+    Args:
+        samples: array of speech samples with shape (N,)
+        winlen: lenght of the analysis window
+        winshift: number of samples to shift the analysis window at every time step
+        preempcoeff: pre-emphasis coefficient
+        nfft: length of the Fast Fourier Transform (power of 2, >= winlen)
+        nceps: number of cepstrum coefficients to compute
+        samplingrate: sampling rate of the original signal
+        liftercoeff: liftering coefficient used to equalise scale of MFCCs
+
+    Returns:
+        N x nceps array with lifetered MFCC coefficients
+    """
+    frames = enframe(samples, winlen, winshift, samplingrate)
+    preemph = preemp(frames, preempcoeff)
+    windowed = windowing(preemph)
+    spec = powerSpectrum(windowed, nfft)
+    mspec = logMelSpectrum(spec, samplingrate)
+    ceps = cepstrum(mspec, nceps)
+    return lifter(ceps, liftercoeff)
+
+
+# Functions to be implemented ----------------------------------
+
+def enframe(samples, winlen=20, winshift=10, samplingrate=20000):
+    """
+    Slices the input samples into overlapping windows.
+
+    Args:
+        winlen: window length in samples.
+        winshift: shift of consecutive windows in samples
+    Returns:
+        numpy array [N x winlen], where N is the number of windows that fit
+        in the input signal
+    """
+
+    winlen = int(winlen * 0.001 * samplingrate)
+    winshift = int(winshift * 0.001 * samplingrate)
+
+    frames = []
+
+    for step in range(0, len(samples), winshift):
+
+        if step + winlen < len(samples):
+            frames.append(samples[step:step + winlen])
+
+    return np.asarray(frames)
+
+
+def preemp(input, p=0.97):
+    """
+    Pre-emphasis filter.
+
+    Args:
+        input: array of speech frames [N x M] where N is the number of frames and
+               M the samples per frame
+        p: preemhasis factor (defaults to the value specified in the exercise)
+
+    Output:
+        output: array of pre-emphasised speech samples
+    Note (you can use the function lfilter from scipy.signal)
+    """
+
+    return lfilter([1, -p], [1], input)
+
+
+def windowing(input):
+    """
+    Applies hamming window to the input frames.
+
+    Args:
+        input: array of speech samples [N x M] where N is the number of frames and
+               M the samples per frame
+    Output:
+        array of windoed speech samples [N x M]
+    Note (you can use the function hamming from scipy.signal, include the sym=0 option
+    if you want to get the same results as in the example)
+    """
+
+    return input * hamming(np.shape(input)[1], sym=False)
+
+
+def powerSpectrum(input, nfft=512):
+    """
+    Calculates the power spectrum of the input signal, that is the square of the modulus of the FFT
+
+    Args:
+        input: array of speech samples [N x M] where N is the number of frames and
+               M the samples per frame
+        nfft: length of the FFT
+    Output:
+        array of power spectra [N x nfft]
+    Note: you can use the function fft from scipy.fftpack
+    """
+
+    return abs(fft(input, n=nfft))
+
+
+def logMelSpectrum(input, samplingrate=20000, plotfilers=False):
+    """
+    Calculates the log output of a Mel filterbank when the input is the power spectrum
+
+    Args:
+        input: array of power spectrum coefficients [N x nfft] where N is the number of frames and
+               nfft the length of each spectrum
+        samplingrate: sampling rate of the original signal (used to calculate the filterbank shapes)
+    Output:
+        array of Mel filterbank log outputs [N x nmelfilters] where nmelfilters is the number
+        of filters in the filterbank
+    Note: use the trfbank function provided in tools.py to calculate the filterbank shapes and
+          nmelfilters
+    """
+
+    filter_bank = trfbank(samplingrate, np.shape(input)[1])
+
+    if plotfilers:
+        plt.plot(filter_bank)
+        plt.xlabel("spectrum length")
+        plt.ylabel("filter amplitudes")
+        plt.title("Mel filters in linear frequency scale")
+        plt.show()
+
+    return np.log(input.dot(filter_bank.T))
+
+
+def cepstrum(input, nceps=13):
+    """
+    Calulates Cepstral coefficients from mel spectrum applying Discrete Cosine Transform
+
+    Args:
+        input: array of log outputs of Mel scale filterbank [N x nmelfilters] where N is the
+               number of frames and nmelfilters the length of the filterbank
+        nceps: number of output cepstral coefficients
+    Output:
+        array of Cepstral coefficients [N x nceps]
+    Note: you can use the function dct from scipy.fftpack.realtransforms
+    """
+    return dct(input)[:, 0:nceps]
+
+
+def dtw(x, y, dist):
+    """Dynamic Time Warping.
+
+    Args:
+        x, y: arrays of size NxD and MxD respectively, where D is the dimensionality
+              and N, M are the respective lenghts of the sequences
+        dist: distance function (can be used in the code as dist(x[i], y[j]))
+
+    Outputs:
+        global_d: global distance between the sequences (scalar) normalized to len(x)+len(y)
+        local_d: local distance between frames from x and y (NxM matrix)
+        acc_d: accumulated distance between frames of x and y (NxM matrix)
+        path: best path thtough AD
+
+    Note that you only need to define the first output for this exercise.
+    """
+    local_d = dist(x, y)
+    global_d = np.zeros(local_d.shape)
+    global_d[0][0] = float("inf")
+
+    for i in range(1, global_d.shape[0]):
+        for j in range(1, global_d.shape[1]):
+            global_d[i][j] = local_d[i][j] + min(global_d[i - 1, j],  # insertion
+                                           global_d[i, j - 1],  # deletion
+                                           global_d[i - 1, j - 1])  # match
+    global_d = global_d[x.shape[0] - 1, y.shape[0] - 1] / (x.shape[0] + y.shape[0])
+    return global_d, local_d
+
+def efc_dist(x, y):
+    """ Compute the Euclidean distance between 2 utterances"""
+
+    dist = np.zeros((x.shape[0], y.shape[0]))
+    for i, value_1 in enumerate(x):
+        for j, value_2 in enumerate(y):
+            dist[i][j] = euclidean(value_1, value_2)
+    return dist
---
+++ src/util.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""Module with audio-data processing tools.
+
+This module contains functions that are used to load and process audio data.
+
+"""
+
+import numpy as np
+import tensorflow as tf
+import os
+from pysndfile import sndio
+import pickle
+from proto import *
+
+__author__ = "MattSt, matsou"
+
+CHECKPOINT_DIR = os.path.join(os.getcwd(), "checkpoints/")
+RESULTS_DIR = os.path.join(os.getcwd(), "results/")
+PARTIAL_RESULTS_DIR = os.path.join(os.getcwd(), "results/training_step_res/")
+
+def get_checkpoint_path(network_name):
+    if not os.path.exists(CHECKPOINT_DIR):
+        os.makedirs(CHECKPOINT_DIR)
+    return os.path.join(CHECKPOINT_DIR, network_name)
+
+def get_results_path(name):
+    if not os.path.exists(RESULTS_DIR):
+        os.makedirs(RESULTS_DIR)
+    return os.path.join(RESULTS_DIR, name)
+
+def get_par_results_path(name):
+    if not os.path.exists(PARTIAL_RESULTS_DIR):
+        os.makedirs(PARTIAL_RESULTS_DIR)
+    return os.path.join(PARTIAL_RESULTS_DIR, name)
+
+def save_pickle_file(filename, data):
+    with open(filename, 'wb') as f:
+        pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_pickle_file(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
+
+def load_audio(file_path):
+    """Loads audio data from wav file using pysndfile.
+
+    Args:
+        file_path: Path to a wav file.
+
+    Returns:
+        A tuple containing the samples and the sampling rate of the
+        wav file, in this order.
+    """
+    data = sndio.read(file_path)
+    sampling_rate = data[1]
+    samples = np.array(data[0], dtype=np.float32)
+    return samples, sampling_rate
+
+
+# TODO: Implement function below
+def crop_or_random_pad(audio_samples, audio_desired_length):
+    """ Preprocesses audio samples by cropping or random padding.
+    
+    Given a fixed signal length, if the given signal is longer than the designated
+    length then this function crops the sides of the signal and if the signal is 
+    smaller than that length it adds random padding until it meets the desired
+    length.
+    
+    Args:
+        audio_samples: A tensor which includes the samples of the signal.
+        audio_desired_length: A tensor with the desired fixed size of the
+            signal, given in that order.
+            
+    Returns:
+        A tf.float32 with the signal after preprocessed so that it has the desired
+        length.
+    """
+    pass
+    # Get tensor with number of samples
+    #sample_no = tf.size(audio_samples)
+    # Get difference of samples with maximum length
+    #diff = tf.subtract(sample_no, audio_desired_length)
+    #abs_diff = tf.abs(diff)
+    # Pad the sides symmetrically 
+    #padded_samples = tf.pad(sample_info, [[0, 0], [0, 0]], mode = 'CONSTANT')
+
+def vizualise_samples(fake, true, itr=None,fs = 16384):
+    fig = plt.figure()
+#     plt.title('The signal')
+    plt.axis('off')
+    plt.subplots_adjust(hspace = 0.4)
+    ax = fig.add_subplot(2,1,1)
+    ax.plot(true)
+    ax.set_title("True")
+    ax = fig.add_subplot(2,1,2)
+    ax.plot(fake)
+    ax.set_title("Fake")
+    save_plot('signal_it{}'.format(itr))
+    plt.show()
+    
+    fig = plt.figure()    
+#     plt.title('The frames')
+    plt.axis('off')
+    plt.subplots_adjust(hspace = 0.4)
+    frames_t = enframe(true, winlen=20, winshift=10, samplingrate=fs)
+    frames_f = enframe(fake, winlen=20, winshift=10, samplingrate=fs)
+    ax = fig.add_subplot(2,1,1)
+    ax.pcolormesh(frames_t)
+    ax.set_title("True")
+    ax = fig.add_subplot(2,1,2)
+    ax.pcolormesh(frames_f)
+    ax.set_title("Fake")
+    save_plot('frames_it{}'.format(itr))
+    plt.show()
+    # 4.2 - Apply pre-emphasis filter to frames
+    fig = plt.figure() 
+    plt.axis('off')
+#     plt.title('The pre-emphasis frames')
+    plt.subplots_adjust(hspace = 0.4)
+    pre_emphasis_t = preemp(frames_t, p=0.97)
+    pre_emphasis_f = preemp(frames_f, p=0.97)
+    ax = fig.add_subplot(2,1,1)
+    ax.pcolormesh(pre_emphasis_t)
+    ax = fig.add_subplot(2,1,2)
+    ax.pcolormesh(pre_emphasis_f)
+    save_plot('pre-emph_it{}'.format(itr))
+    plt.show()
+    # 4.3 - Apply hamming window to the pre-emphasized frames
+    fig = plt.figure() 
+    plt.axis('off')
+#     plt.title('The windowed frames')
+    plt.subplots_adjust(hspace = 0.4)
+    windowed_t = windowing(pre_emphasis_t)
+    windowed_f = windowing(pre_emphasis_f)
+    ax = fig.add_subplot(2,1,1)
+    ax.pcolormesh(windowed_t)
+    ax = fig.add_subplot(2,1,2)
+    ax.pcolormesh(windowed_f)
+    save_plot('window_it{}'.format(itr))
+    plt.show()
+    # 4.4 - Compute the power spectrum of the windowed frames
+    fig = plt.figure()  
+    plt.axis('off')
+#     plt.title('The power spectrogram of the windowed frames')
+    plt.subplots_adjust(hspace = 0.4)
+    power_spec_t = powerSpectrum(windowed_t, nfft=512)
+    power_spec_f = powerSpectrum(windowed_f, nfft=512)
+    ax = fig.add_subplot(2,1,1)
+    ax.pcolormesh(power_spec_t)
+    ax = fig.add_subplot(2,1,2)
+    ax.pcolormesh(power_spec_f)
+    save_plot('spectrogram_it{}'.format(itr))
+    plt.show()
+
+def save_plot(savename):
+    savename = "plot_out" if savename is None else savename
+    filepath = 'results'
+    if savename is not None:
+        savename += '.png'
+        savepath = os.path.abspath(os.path.join(os.pardir, filepath))
+        if not os.path.exists(savepath):
+            os.makedirs(savepath)
+        plt.savefig(os.path.join(savepath, savename), bbox_inches='tight')
+        print(savename + " successfully saved to " + filepath)
+
